9d06e28bdd19af3e589826e06340cf64c606c72e
EITP <56854069+EITP@users.noreply.github.com>
2019-11-27 12:18:43 +0800

    Delete introduction.jsx

--- Web Development/app/steps/introduction.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint react/prop-types: 0 */
-import { step } from 'govhk-form-core';
-import logo from './logo.svg';
-
-export default step.introduction({
-  departmentName: ({ i }) => i`departmentName`,
-  logo,
-});
