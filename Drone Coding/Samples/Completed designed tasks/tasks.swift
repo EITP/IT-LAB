bcf7ee264e9076277e6cb81e7eaaa6fba2344d04
EITP <56854069+EITP@users.noreply.github.com>
2020-03-31 14:30:54 +0800

    Create tasks.swift

---
+++ Drone Coding/Samples/Completed designed tasks/tasks.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var WiFiImageView: UIImageView!
+    
+    let tello = Tello()
+
+    // MARK: - IBActions
+    
+    @IBAction func takeOffTapped(_ sender: UIButton) {
+        switch tello.state {
+        case .disconnected:
+            showNoWiFiAlert()
+            break
+        case .wifiUp:
+            tello.enterCommandMode()
+            tello.takeOff()
+            break
+        case .command:
+            tello.takeOff()
+            break
+        }
+    }
+    
+    @IBAction func landTapped(_ sender: UIButton) {
+        tello.land()
+    }
+    
+    @IBAction func stopTapped(_ sender: UIButton) {
+        tello.stop()
+    }
+    
+    
+    @IBAction func settingsTapped(_ sender: UIButton) {
+    }
+    
+    // MARK: - View Management
+    
+    func showNoWiFiAlert() {
+        let alert = UIAlertController(title: "Not Connected to Tello WiFi", message: "In order to control the Tello you must be connected to its WiFi network. Turn on the Tello and then go to Settings -> WiFi to connect.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Check if connected to Tello WiFi
+        let ssidArray = currentSSID()
+        
+        if connectedToSSID(ssidArray: ssidArray, SSID: "TELLO") {
+            tello.state = .wifiUp
+            self.WiFiImageView.image = UIImage(named: "WiFi100")
+            print("Connected to Tello WiFi.")
+        }
+        else {
+            self.WiFiImageView.image = UIImage(named: "WiFiDisconnected")
+            showNoWiFiAlert()
+        }
+    }
+
+}
