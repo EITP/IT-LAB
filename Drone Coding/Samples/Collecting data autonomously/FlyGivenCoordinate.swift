4f2cde16f6f37805d59bc89eb93912eaf7f1ed15
EITP <56854069+EITP@users.noreply.github.com>
2020-04-29 12:42:23 +0800

    Create FlyGivenCoordinate.swift

---
+++ Drone Coding/Samples/Collecting data autonomously/FlyGivenCoordinate.swift
@@ -0,0 +1,72 @@
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var WiFiImageView: UIImageView!
+    
+    let tello = Tello()
+    
+    @IBAction func takeOffTapped(_ sender: UIButton) {
+        switch tello.state {
+        case .disconnected:
+            showNoWiFiAlert()
+            break
+        case .wifiUp:
+            tello.enterCommandMode()
+            tello.takeOff()
+            break
+        case .command:
+            tello.takeOff()
+            var padId = getPadID()
+            getPadPos()
+            flyLine(x: -3, y: -3, z: 80, pad: padId)
+            getPadPos()
+            land()
+            
+            break
+        }
+    }
+    
+    @IBAction func landTapped(_ sender: UIButton) {
+        tello.land()
+    }
+    
+    @IBAction func stopTapped(_ sender: UIButton) {
+        tello.stop()
+    }
+    
+    
+    @IBAction func settingsTapped(_ sender: UIButton) {
+    }
+    
+    
+    func showNoWiFiAlert() {
+        let alert = UIAlertController(title: "Not Connected to Tello WiFi", message: "In order to control the Tello you must be connected to its WiFi network. Turn on the Tello and then go to Settings -> WiFi to connect.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+
+        let ssidArray = currentSSID()
+        
+        if connectedToSSID(ssidArray: ssidArray, SSID: "TELLO") {
+            tello.state = .wifiUp
+            self.WiFiImageView.image = UIImage(named: "WiFi100")
+            print("Connected to Tello WiFi.")
+        }
+        else {
+            self.WiFiImageView.image = UIImage(named: "WiFiDisconnected")
+            showNoWiFiAlert()
+        }
+    }
+
+}
